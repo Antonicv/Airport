63ef4d181dd6b871d0b20d43820f6ce518271e0d
Toni <antonicv.88@gmail.com>
2025-04-06 21:21:07 +0200

    Implement CORS configuration and update controllers for CORS support

--- src/main/java/com/example/AirportApp/AirportApplication.java
+++ src/main/java/com/example/AirportApp/AirportApplication.java
@@ -10,3 +10,5 @@ public class AirportApplication {
 		SpringApplication.run(AirportApplication.class, args);
 	}
 }
+
+
--- src/main/java/com/example/AirportApp/controllers/CrewMemberController.java
+++ src/main/java/com/example/AirportApp/controllers/CrewMemberController.java
@@ -5,6 +5,9 @@ import com.example.AirportApp.service.CrewMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
